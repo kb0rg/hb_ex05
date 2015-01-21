43feaddb2be11f060e92d2cec3e5cb0802a00570
Hackbright Student <info@hackbrightacademy.com>
2015-01-20 18:09:07 -0800

    exercise info without any code written yet.

---
+++ ex05.py
@@ -0,0 +1,27 @@
+"""
+Write a program, lettercount.py, that opens a file named on the command line and counts how many times each letter occurs in that file. Your program should then print those counts to the screen, in alphabetical order. For example:
+
+inputfile.txt:
+An abacus
+
+$ python lettercount.py inputfile.txt
+3
+1
+1
+0
+0
+...
+Note: you may not use dictionaries in your program for this exercise.
+
+We have provided a file 'twain.txt' for you to test your code on.
+
+"""
+
+
+
+"""
+Once you've produced appropriate output, you can visualize it by piping the contents of your program to the 'spark' utility installed on your computer:
+
+$ python lettercount.py inputfile.txt | spark
+▃▂▄▁▇▅█▁▇▂▁▁▅▃▅▇▁▄▄▂▂▇▆▄▂▄
+"""
